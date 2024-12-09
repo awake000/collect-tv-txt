33b915b64c27f8e18bcb0a40a66b280651a2d1de
awake000 <81493284+awake000@users.noreply.github.com>
2024-12-09 17:01:24 +0800

    Update new.py

--- ipv4直播源/new.py
+++ ipv4直播源/new.py
@@ -391,7 +391,7 @@ results.sort(key=lambda x: channel_key(x[0]))
 
 result_counter = 8  # 每个频道需要的个数
 
-with open("itvlist.txt", 'w', encoding='utf-8') as file:
+with open("ipv4.txt", 'w', encoding='utf-8') as file:
     channel_counters = {}
     #file.write('央视频道,#genre#\n')
     for result in results:
@@ -435,7 +435,7 @@ with open("itvlist.txt", 'w', encoding='utf-8') as file:
                 file.write(f"{channel_name},{channel_url}\n")
                 channel_counters[channel_name] = 1
 
-with open("itvlist.m3u", 'w', encoding='utf-8') as file:
+with open("ipv4.m3u", 'w', encoding='utf-8') as file:
     channel_counters = {}
     file.write('#EXTM3U\n')
     for result in results:
@@ -445,11 +445,11 @@ with open("itvlist.m3u", 'w', encoding='utf-8') as file:
                 if channel_counters[channel_name] >= result_counter:
                     continue
                 else:
-                    file.write(f"#EXTINF:-1 group-title=\"央视频道\",{channel_name}\n")
+                    #file.write(f"#EXTINF:-1 group-title=\"央视频道\",{channel_name}\n")
                     file.write(f"{channel_url}\n")
                     channel_counters[channel_name] += 1
             else:
-                file.write(f"#EXTINF:-1 group-title=\"央视频道\",{channel_name}\n")
+                #file.write(f"#EXTINF:-1 group-title=\"央视频道\",{channel_name}\n")
                 file.write(f"{channel_url}\n")
                 channel_counters[channel_name] = 1
     channel_counters = {}
@@ -461,11 +461,11 @@ with open("itvlist.m3u", 'w', encoding='utf-8') as file:
                 if channel_counters[channel_name] >= result_counter:
                     continue
                 else:
-                    file.write(f"#EXTINF:-1 group-title=\"卫视频道\",{channel_name}\n")
+                    #file.write(f"#EXTINF:-1 group-title=\"卫视频道\",{channel_name}\n")
                     file.write(f"{channel_url}\n")
                     channel_counters[channel_name] += 1
             else:
-                file.write(f"#EXTINF:-1 group-title=\"卫视频道\",{channel_name}\n")
+                #file.write(f"#EXTINF:-1 group-title=\"卫视频道\",{channel_name}\n")
                 file.write(f"{channel_url}\n")
                 channel_counters[channel_name] = 1
     channel_counters = {}
@@ -477,10 +477,10 @@ with open("itvlist.m3u", 'w', encoding='utf-8') as file:
                 if channel_counters[channel_name] >= result_counter:
                     continue
                 else:
-                    file.write(f"#EXTINF:-1 group-title=\"其他频道\",{channel_name}\n")
+                    #file.write(f"#EXTINF:-1 group-title=\"其他频道\",{channel_name}\n")
                     file.write(f"{channel_url}\n")
                     channel_counters[channel_name] += 1
             else:
-                file.write(f"#EXTINF:-1 group-title=\"其他频道\",{channel_name}\n")
+                #file.write(f"#EXTINF:-1 group-title=\"其他频道\",{channel_name}\n")
                 file.write(f"{channel_url}\n")
                 channel_counters[channel_name] = 1
